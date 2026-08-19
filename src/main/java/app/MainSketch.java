728e22628d18f46dcf9c5118e9c894b7a19ee746
Alperen Yilmaz <alperen.yilmaz@students.fhnw.ch>
2026-08-19 18:24:41 +0200

    feat(MainSketch): implement initial window size and font setup

--- src/main/java/app/MainSketch.java
+++ src/main/java/app/MainSketch.java
@@ -3,6 +3,7 @@ package app;
 import game.Difficulty;
 import game.GameManager;
 import processing.core.PApplet;
+import processing.core.PFont;
 import processing.sound.SoundFile;
 import settings.GameSettings;
 
@@ -15,18 +16,32 @@ public class MainSketch extends PApplet {
     private Difficulty selectedDifficulty;
     private long countdownStartMs;
     private int tutorialPage;
+    private PFont font;
+
+    private final int WIDTH = 1280;
+    private final int HEIGHT = 720;
 
     @Override
     public void settings() {
-        fullScreen();
+        size(WIDTH, HEIGHT);
     }
 
     @Override
     public void setup() {
+        font = createFont("Torus", 32);
+        textFont(font);
     }
 
     @Override
     public void draw() {
+        background(0);
+
+        // drawLanes();
+        // drawNotes();
+
+        fill(255);
+        text("改善\nwelcome to kaizen!", (float) WIDTH / 2, 120);
+        textAlign(CENTER);
     }
 
     public static void main(String[] args) {
