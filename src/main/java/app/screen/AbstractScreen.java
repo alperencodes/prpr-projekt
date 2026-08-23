d14a80ee54f9607d30f7897c084f7e070500be8c
Alperen Yilmaz <alperen.developer@icloud.com>
2026-08-23 17:21:36 +0200

    feat(AbstractScreen): add abstract class for screen management

---
+++ src/main/java/app/screen/AbstractScreen.java
@@ -0,0 +1,27 @@
+package app.screen;
+
+import controlP5.ControlP5;
+import controlP5.Group;
+
+public abstract class AbstractScreen implements Screen {
+    protected final Group group;
+
+    protected AbstractScreen(ControlP5 cp5, String groupName) {
+        group = cp5.addGroup(groupName).setLabel("");
+    }
+
+    @Override
+    public void show() {
+        group.show();
+    }
+
+    @Override
+    public void hide() {
+        group.hide();
+    }
+
+    @Override
+    public void draw() {
+        // Most screens currently only contain ControlP5 components.
+    }
+}
